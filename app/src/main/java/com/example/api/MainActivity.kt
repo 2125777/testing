895248019504813c272eb07c88bc17aa19940514
akmdaiman <awangkumuhdaiman@gmail.com>
2023-10-11 14:59:46 +0800

    Initial Commit

--- app/src/main/java/com/example/api/MainActivity.kt
+++ app/src/main/java/com/example/api/MainActivity.kt
@@ -1,65 +1,101 @@
-package com.example.api
+package com.example.apicallvolley
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import com.example.apicallvolley.R
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
 import com.example.apicallvolley.databinding.ActivityMainBinding
 import org.json.JSONArray
+import org.json.JSONObject
+import java.io.IOException
 import java.net.HttpURLConnection
 import java.net.URL
 import java.util.Scanner
 
-
+private lateinit var binding : ActivityMainBinding
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding : ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnQuote.setOnClickListener {
-            getQuote(binding.textView)
+        binding.btnQuote.setOnClickListener(){
+            handleRetrieveQuoteWithVolley()
         }
+
     }
+    fun getQuote(view: View) {
+        val thread = Thread {
 
-    fun getQuote(view: View) { val thread = Thread {
-        val url = URL("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
+            val url = URL("https://ron-swanson-quotes.herokuapp.com/v2/quotes ")
 
-        val connection = url.openConnection() as HttpURLConnection
-        connection.connectTimeout = 10000
-        connection.readTimeout = 10000
-        connection.requestMethod = "GET"
-        connection.connect()
+            try {
+                val connection = url.openConnection()
+                if (connection is HttpURLConnection) {
+                    connection.connectTimeout = 10000
+                    connection.readTimeout = 10000
+                    connection.requestMethod = "GET"
+                    connection.connect()
 
-        val responseCode = connection.responseCode
+                    val responseCode = connection.responseCode
 
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            val scanner = Scanner(connection.inputStream)
-            scanner.useDelimiter("\\A")
-            val jsonData = if (scanner.hasNext()) scanner.next() else ""
-            Log.d("jsonData", jsonData)
-            val jsonArray = JSONArray(jsonData)
-            val quote = jsonArray[1].toString()
-            updateTextBoxFromThread(quote)
+                    if (responseCode == HttpURLConnection.HTTP_OK) {
+                        val scanner = Scanner(connection.inputStream)
+                        scanner.useDelimiter("\\A")
+                        val jsonData = if (scanner.hasNext()) scanner.next() else ""
+                        Log.d("jsondata", jsonData)
+                        val jsonArray = JSONArray(jsonData)
+                        val quote = jsonArray[0].toString() // only grab one item the 0
+                        updateTextBoxFromThread(quote)
+                        Log.d("jsonArray[0]", quote)
+
+
+                    } else {
+                        updateTextBoxFromThread("Sorry, there's a problem with the server")
+                    }
+
+                } else {
+                    Log.wtf("MAIN_ACTIVITY", "Someone changed the API protocol")
+                }
+            } catch (e: IOException) {
+                updateTextBoxFromThread("Sorry, there was an error processing the data")
+            }
         }
-    }
         thread.start()
-    }
 
-    private fun parseJSON (jsonData: String?): String{
-        val jsonArray = JSONArray(jsonData)
-        val quote = jsonArray[0].toString()
+    }
+    private fun parseJson(jsonData: String?): String {
+        val jsonObject = JSONObject(jsonData)
+        val quote = jsonObject.getString("value")
         return quote
     }
-    private fun updateTextBoxFromThread (text: String){
-        runOnUiThread{
+
+    private fun updateTextBoxFromThread(text: String) {
+        runOnUiThread {
             binding.textView.text = text
         }
+
     }
+    private fun handleRetrieveQuoteWithVolley() {
+        val queue = Volley.newRequestQueue(this)
+
+        val jsonObjectRequest = JsonArrayRequest(
+            Request.Method.GET, "https://ron-swanson-quotes.herokuapp.com/v2/quotes ", null,
+            { response -> binding.textView.text = parseJson(response.toString()) },
+            { binding.textView.text = "That didn't work!" + ": $it"}
+        )
+        queue.add(jsonObjectRequest)
+
+
+    }
+
+
+
+
+
+
 }
